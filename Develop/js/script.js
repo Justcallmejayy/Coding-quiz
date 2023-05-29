b9c8906553c65651889f24577ae07cf8c4a190fb
Justcallmejayy <130424384+Justcallmejayy@users.noreply.github.com>
2023-05-29 16:29:57 -0700

    styled playAgainBtn, submitbtn,

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -7,6 +7,7 @@ let highscoresContainer = document.querySelector('.highscores')
 let submitBtn = document.getElementById('submitBtn')
 let playAgainBtn = document.getElementById('play-again')
 let highscoresList = document.getElementById('highscores-list')
+let clickHighscores = document.getElementById('highscores')
 let interval;
 let currentQuestionIndex = 0
 let timer = 60
@@ -14,39 +15,37 @@ let score = 0
 
 
 let quizQuestion = [
-    {question: 'What is an array?',
-    options: ['A string', 'option2','option3', 'option4'],
-    answer: 'A string'
+    {question: 'What do you type first before declaring a variable?',
+    options: ['For/While/If', 'Let/Var/Const','If/For/Var', 'It/Be/Yes'],
+    answer: 'Let/Var/Const'
     },
     {question: 'What is a boolean?',
-    options: ['True/False', 'option2','option3', 'option4'],
+    options: ['True/False', 'On/Off','Left/Right', 'Up/Down'],
     answer: 'True/False'
     },
-    {question: 'Do you need parantheses or brackets for an array?',
-    options: ['option 1', 'option2','option3', 'option4'],
-    answer: 'option 1'
+    {question: 'How do you call a function?',
+    options: ['[]', '{}','()', '<>'],
+    answer: '()'
     },
-    {question: 'What are for loops?',
-    options: ['option 1', 'option2','option3', 'option4'],
-    answer: 'option 1'
+    {question: 'What is the API name for a timer?',
+    options: ['setInterval[]', 'setInterval()', 'setTimer[]', 'startTimer()'],
+    answer: 'setInterval()'
     },]
+
 
 function startTimer() {
     interval = setInterval(function() {
         timer--;
-        timeContainer.textContent = timer
+        timeContainer.textContent = 'Timer:' + timer
         if (timer === 0) {
-            endQuiz();
-        }
-    }, 1000) 
+            endQuiz(); 
+        } 
+         }, 1000) 
         
 }
 
     
     function startQuiz() {
- /*   if (highscoresTextContainer.className === 'hide') {
-        return; 
-    } */
     startBtn.style.display = 'none';
     submitBtn.className = 'hide'
     displayQuestion();
@@ -55,25 +54,27 @@ function startTimer() {
     
 }
     let questionContainer = document.getElementById('question-container')
-function endQuiz() {
-    questionContainer.innerHTML = 'Thanks for Playing you got ' + score + ' right!' + ' write your intials and add to the highscore list!'
+    function endQuiz() {
+    questionContainer.innerHTML = 'Thanks for playing you got ' + score + ' right' + ' in '+ timer + ' seconds!' + ' Write your intials and add to the highscore list!'
+    questionContainer.classList.add('endGame')
     wrongAnswerContainer.innerHTML = ""
     clearInterval(interval);
     highscoresContainer.classList.remove('highscores')
-    highscoresContainer.className += ' displayHighscores'
+    highscoresContainer.classList.add('displayHighscores') 
     playAgainBtn.classList.remove('hide')
     highscoresTextContainer.classList.remove('hide')
     submitBtn.classList.remove('hide')
 
   }
 
-function displayQuestion() {
+    function displayQuestion() {
     if (currentQuestionIndex === quizQuestion.length) {
         endQuiz();
         return
     }
     let question = quizQuestion[currentQuestionIndex].question
-    let questionEl = document.createElement('h2')
+    let questionEl = document.createElement('div')
+    questionEl.classList.add('h2')
    // This is how to grab from object quizQuestions, and display it onto the page
     questionContainer.innerHTML = ''
     questionEl.textContent = question
@@ -84,9 +85,10 @@ function displayQuestion() {
 
     for (i=0; i < options.length; i++) {
         let optionsEl = document.createElement('button')
+        optionsEl.classList.add('optionBtn')
         optionsEl.addEventListener('click', selectOption)    // add this back after defining selectOption
         optionsEl.textContent = options[i]
-        questionEl.appendChild(optionsEl)
+        questionContainer.appendChild(optionsEl)
     }
     currentQuestionIndex ++
    
@@ -96,7 +98,6 @@ function selectOption(event) {
         let selectedOption = event.target.textContent
         let wrongAnswerEl = document.createElement('div')
         let answer = quizQuestion[currentQuestionIndex - 1].answer
-        console.log(answer)
         if (selectedOption === answer) {
         wrongAnswerContainer.innerHTML = ""
         score++
@@ -105,6 +106,7 @@ function selectOption(event) {
     if (selectedOption !== answer) {
         wrongAnswerContainer.innerHTML = ""
         wrongAnswerEl = document.createElement('div')
+        wrongAnswerEl.classList.add('incorrect')
         wrongAnswerEl.textContent = "Incorrect"
         wrongAnswerContainer.appendChild(wrongAnswerEl)
         displayQuestion()
@@ -112,28 +114,32 @@ function selectOption(event) {
     
 }
 
-submitBtn.addEventListener('click', function() {
-  let intials = highscoresTextContainer.value
-  let highscores = JSON.parse(localStorage.getItem('highscores')) || []
-  let userScores = {
-    initals: intials,
-    score: score
-  }
-    highscores.push(userScores)
-    localStorage.setItem('highscores',JSON.stringify(highscores))
-    highscoresTextContainer.value = ''
-    highscoresTextContainer.className = ' hide'
-    submitBtn.className = 'hide'
-    questionContainer.innerHTML = ''
-  
-  //  if (highscores.length > 0) {
-    highscoresList.innerHTML = '';
-    for (i = 0; i < highscores.length; i++) {
-        let li = document.createElement('li');
-        li.textContent = `${highscores[i].initals}: ${highscores[i].score}`;
-        highscoresList.appendChild(li);
+function displayHighscores() {
+    let intials = highscoresTextContainer.value
+    let highscores = JSON.parse(localStorage.getItem('highscores')) || []
+    let userScores = {
+      initals: intials,
+      score: score,
+      time: timer
     }
-})
+      highscores.push(userScores)
+      localStorage.setItem('highscores',JSON.stringify(highscores))
+      highscoresTextContainer.value = ''
+      highscoresTextContainer.className = ' hide'
+      submitBtn.className = 'hide'
+      questionContainer.innerHTML = ''
+      questionContainer.classList.remove('endGame')
+    
+    //  if (highscores.length > 0) {
+      highscoresList.innerHTML = '';
+      for (i = 0; i < highscores.length; i++) {
+          let li = document.createElement('li');
+          li.textContent = `${highscores[i].initals}: ${highscores[i].score} ${highscores[i].time}`;
+          highscoresList.appendChild(li);
+      }
+}
+
+submitBtn.addEventListener('click', displayHighscores)
 
 // Make a button that will restart quiz
 playAgainBtn.addEventListener('click', function() {
@@ -145,5 +151,23 @@ playAgainBtn.addEventListener('click', function() {
     highscoresList.textContent = ''
     highscoresTextContainer.value = '';
     highscoresTextContainer.className = 'hide';
-    
+    questionContainer.classList.remove('endGame')
 })
+
+clickHighscores.addEventListener('click', function() {
+    let highscores = JSON.parse(localStorage.getItem('highscores')) || []
+      highscoresContainer.classList.remove('hide')
+      questionContainer.innerHTML = ''
+      questionContainer.classList.remove('endGame')
+      
+    
+    //  if (highscores.length > 0) {
+      highscoresList.innerHTML = '';
+      for (i = 0; i < highscores.length; i++) {
+          let li = document.createElement('li');
+          li.textContent = `${highscores[i].initals}: ${highscores[i].score}`;
+          highscoresList.appendChild(li);
+        }
+      })
+
+
